fa1dbbc69a4afea59b34c301f5c45d63672ebfde
João Carlos Ferreira Marques <joaocarlusferrera@gmail.com>
2025-08-11 16:25:28 -0300

    feat: initialize bucket items in ViewModel

--- app/src/main/java/com/krllus/bitmappoc/bucketlist/BucketViewModel.kt
+++ app/src/main/java/com/krllus/bitmappoc/bucketlist/BucketViewModel.kt
@@ -10,6 +10,14 @@ class BucketViewModel : ViewModel() {
     private val _items = mutableStateListOf<BucketItem>()
     val items: SnapshotStateList<BucketItem> = _items
 
+    init {
+        _items.add(BucketItem("Skydiving", 1200.00))
+        _items.add(BucketItem("Visit the Grand Canyon", 500.50))
+        _items.add(BucketItem("Learn to play Guitar", 150.75))
+        _items.add(BucketItem("Write a Book", 50.00))
+        _items.add(BucketItem("Run a Marathon", 200.00))
+    }
+
     fun addItem(name: String, amount: Double) {
         _items.add(BucketItem(name, amount))
     }
